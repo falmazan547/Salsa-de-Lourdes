adb698c5017fe21f907e79c39e0b72e9a309b6e8
falma <falma@DESKTOP-SOPFOVF>
2019-02-18 17:04:05 -0600

    New Site

---
+++ SalsaDeLourdes/SalsaDeLourdes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalsaDeLourdes.Startup))]
+namespace SalsaDeLourdes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
